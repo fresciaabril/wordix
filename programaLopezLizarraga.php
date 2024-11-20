534a7478dcbcc94798f4a3c110ab6ac71edb3ad6
vero-1770 <verolm2006@gmail.com>
2024-11-20 17:11:56 -0300

    hola

--- programaLopezLizarraga.php
+++ programaLopezLizarraga.php
@@ -31,10 +31,6 @@ function cargarColeccionPalabras()
     return ($coleccionPalabras);
 }
 
-/* ****COMPLETAR***** */
-
-
-
 /**************************************/
 /*********** PROGRAMA PRINCIPAL *******/
 /**************************************/
