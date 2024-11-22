9f6071dbc4d7d058c439b057cb0250d2f8778bbb
fresciaabril <lizarragafrescia@gmail>
2024-11-22 02:56:29 -0300

    nuevo borrador

--- borrador/programaapellidos.php
+++ borrador/programaapellidos.php
@@ -32,55 +32,78 @@ function cargarColeccionPalabras()
 }
 
 
-/** Una función llamada cargarPartidas, que inicialice una estructura de datos con ejemplos de Partidas y que
- * retorne la colección de partidas descripta en la sección EXPLICACION 2. Mínimo debe cargar 10 partidas
-* donde vayan variando los jugadores, las palabras, los intentos y los puntajes, en algunos casos los
- *jugadores se deben repetir.
- * 
+/** 
+ * Esta funcion muestra ejemplos de partidas
+ *@return array
  */
-function cargarPartidas( ){
+    function cargarPartidas (){
+    //INT $i, array $ejemploPartidas
+    $cantPart=10;
+    $ejemploPartidas[0]= ["palabraWordix "=> "QUESO" , "jugador" => "maria", "intentos"=> 0, "puntaje" => 0];
+    $ejemploPartidas[1]= ["palabraWordix "=> "MUJER" , "jugador" => "luis", "intentos"=> 1, "puntaje" => 15];
+    $ejemploPartidas[2]= ["palabraWordix "=> "SILLA" , "jugador" => "micaela", "intentos"=> 3, "puntaje" => 13];
+    $ejemploPartidas[3]= ["palabraWordix "=> "LIBRO" , "jugador" => "valeria", "intentos"=> 6, "puntaje" => 10];
+    $ejemploPartidas[4]= ["palabraWordix "=> "FUEGO" , "jugador" => "rocio", "intentos"=> 0, "puntaje" => 0];
+    $ejemploPartidas[5]= ["palabraWordix "=> "CASAS" , "jugador" => "maria", "intentos"=> 4, "puntaje" => 13];
+    $ejemploPartidas[6]= ["palabraWordix "=> "RASGO" , "jugador" => "pablo", "intentos"=> 1, "puntaje" => 16];
+    $ejemploPartidas[7]= ["palabraWordix "=> "VERDE" , "jugador" => "maximo", "intentos"=> 0, "puntaje" => 0];
+    $ejemploPartidas[8]= ["palabraWordix "=> "MELON" , "jugador" => "federico", "intentos"=> 5, "puntaje" => 10];
+    $ejemploPartidas[9]= ["palabraWordix "=> "RATAS" , "jugador" => "abril", "intentos"=> 2, "puntaje" => 16];
+    $ejemploPartidas[10]= ["palabraWordix "=> "TINTO" , "jugador" => "lucas", "intentos"=> 4, "puntaje" => 13];
+    for($i = 0;  $i < $cantPart;  $i++){ 
+        echo "partidas: \n " . ($i+1)  . $ejemploPartidas[$i]["palabrawordix "]  .  $ejemploPartidas[$i]["jugador"] .
+         $ejemploPartidas[$i]["intentos"] . $ejemploPartidas[$i]["puntaje"]. "\n";
+    }
+    return $ejemploPartidas;
+    }
 
 
-}
 
 
 
-
-
-
-/** Unafunción que le pida al usuario ingresar una palabra de 5 letras, y retorne la palabra.
- * 
- */
-function retornarPalabra(){
-
-
-}
 
 
 /**Una función que dado un número de partida,muestre en pantalla los datos de la partida como lo indica la
  sección EXPLICACIÓN 1
  * 
  */
-function datosPartida(){
+    function mostrarDatosPartida(){
+        //INT $numPart,
+        echo "Ingrese un el numero de partida que desea ver: \n";
+        $numPart=trim(fgets(STDIN));
+
+    }
 
 
-}
 
-/** Una función que dada una colección de par das y el nombre de un jugador, retorne el índice de la primer
- *par da ganada por dicho jugador. Si el jugador ganó ninguna par da, la función debe retornar el valor-1.
- *(debe u lizar las instrucciones vistas en la materia, no u lizar funciones predefinidas de php).
+/** Una función que dada una colección de partidas y el nombre de un jugador, retorne el índice de la primer
+ *partida ganada por dicho jugador. Si el jugador ganó ninguna partida, la función debe retornar el valor-1.
+ *(debe utilizar las instrucciones vistas en la materia, no utilizar funciones predefinidas de php).
  * 
  */
-function primerPartidaGanada(){
-
-
-}
+    function primerPartidaGanada($coleccionPartidas, $nombre){
+        //INT $primerPartG,$i
+        $gano=-01;
+        $i=0; //el i es la variable de numero de la coleccion
+        $partGan=false;
+        while ($partGan && $coleccionPartidas[$i]["ganadas"]!= 0)
+        if($coleccionPartidas[$i]["nombre"] == $nombre){
+            $gano="indice";
+            
+        }
+        return $gano;
+    }
 
 /**Una función solicitarJugador sin parámetros formales que solicite al usuario el nombre de un jugador y
  retorne el nombre en minúsculas. La función debe asegurar que el nombre del jugador comience con una
  letra. (Utilice funciones predefinidas de string).
  * 
  */
+    function solicitarJugador(){
+        echo "Ingrese el nombre de un jugador ";
+        $nomJugador=trim(fgets(STDIN));
+        strtolower(string:$nomJugador);
+    }
 
 /* ****COMPLETAR***** */
 
@@ -142,3 +165,7 @@ do {
     }
 } while ($opcion != 8);
 */
+
+
+ cargarPartidas();
+   
